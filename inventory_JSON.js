81c88fecd94d7b00558b2a46b8c0c82400dc7236
Jordan <jlfvff@mail.umsl.edu>
2019-12-07 20:30:56 -0600

    Cables galore

--- inventory_JSON.js
+++ inventory_JSON.js
@@ -23,5 +23,93 @@ let inventory_JSON =
         productPrice: "399.00",
         productCategory: "headphones",
         productImage: "./resources/BoseNCheadphones.jpg"
-    }
+    },
+    {
+        productID: "4",
+        productName: "Lightning to USB A cable - 6 feet",
+        productBrand: "AmazonBasics",
+        productPrice: "12.99",
+        productCategory: "cables",
+        productImage: "./resources/USB_A.jpg"
+    },
+    {
+        productID: "5",
+        productName: "RJ45 Cat-6 Ethernet Patch Internet Cable - 5 Feet",
+        productBrand: "Amazon Basics",
+        productPrice: "4.53",
+        productCategory: "cables",
+        productImage: "./resources/ethernet_5ft.jpg"
+    },
+    {
+        productID: "6",
+        productName: "USB to USB Extension Cable - 10 feet",
+        productBrand: "Cable Matters",
+        productPrice: "7.99",
+        productCategory: "cables",
+        productImage: "./resources/USB_extension.jpg"
+    },
+    {
+        productID: "7",
+        productName: "Cat8 Ethernet Cable - 6 feet",
+        productBrand: "DbillionDa",
+        productPrice: "7.99",
+        productCategory: "cables",
+        productImage: "./resources/ethernet_6ft.jpg"
+    },
+    {
+        productID: "8",
+        productName: "DisplayPort to DisplayPort Cable - 10 feet",
+        productBrand: "Cable Matters",
+        productPrice: "12.99",
+        productCategory: "cables",
+        productImage: "./resources/displayport_10ft.jpg"
+    },
+    {
+        productID: "9",
+        productName: "USB-C Charge Cable",
+        productBrand: "Apple",
+        productPrice: "35.00",
+        productCategory: "cables",
+        productImage: "./resources/appleUSB_C.jpg"
+    },
+    {
+        productID: "10",
+        productName: "Lightning to USB Charge Cable",
+        productBrand: "Apple",
+        productPrice: "29.00",
+        productCategory: "cables",
+        productImage: "./resources/applelightningtoUSBC.jpg"
+    },
+    {
+        productID: "11",
+        productName: "Unidirectional DisplayPort to HDMI Adapter Cable",
+        productBrand: "Cable Matters",
+        productPrice: "10.99",
+        productCategory: "cables",
+        productImage: "./resources/unidirectional_displayport.jpg"
+    },
+    {
+        productID: "12",
+        productName: "HDMI to HDMI Cable - 6 feet",
+        productBrand: "Cable Matters",
+        productPrice: "8.99",
+        productCategory: "cables",
+        productImage: "./resources/HDMItoHDMI_6ft.jpg"
+    },
+    {
+        productID: "13",
+        productName: "2-Pack XLR to XLR Microphone Cable - 3 feet",
+        productBrand: "Cable Matters",
+        productPrice: "12.99",
+        productCategory: "cables",
+        productImage: "./resources/XLRtoXLR_2pack.jpg"
+    },
+    {
+        productID: "14",
+        productName: "2-Pack Ultra Thin HDMI Cable - 6 feet",
+        productBrand: "Cable Matters",
+        productPrice: "12.99",
+        productCategory: "cables",
+        productImage: "./resources/UltraThinHDMI_2pack.jpg"
+    },
 ];
